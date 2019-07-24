2edfe5506a5761905a834fa1b1af07aa05a2d4fe
Tabrielluu <35918277+Gabriel-Lu@users.noreply.github.com>
2019-07-24 14:09:53 +0800

    added comments

--- cnews_group.py
+++ cnews_group.py
@@ -19,9 +19,10 @@ import os
 def _read_file(filename):
     """读取一个文件并转换为一行"""
     with open(filename, 'r', encoding='utf-8') as f:
-        return f.read().replace('\n', '').replace('\t', '').replace('\u3000', '') #删除掉了：换行;缩进;全角空白符
+        return f.read().replace('\n', '').replace('\t', '').replace('\u3000', '')  # 删除掉了：换行;缩进;全角空白符
 
-def save_file(dirname):#data/thucnews
+
+def save_file(dirname):  # data/thucnews
     """
     将多个文件整合并存到3个文件中
     dirname: 原数据目录
@@ -30,9 +31,9 @@ def save_file(dirname):#data/thucnews
     f_train = open('data/cnews/cnews.train.txt', 'w', encoding='utf-8')
     f_test = open('data/cnews/cnews.test.txt', 'w', encoding='utf-8')
     f_val = open('data/cnews/cnews.val.txt', 'w', encoding='utf-8')
-    for category in os.listdir(dirname):   # 分类目录
+    for category in os.listdir(dirname):  # 分类目录
         print(category)
-        cat_dir = os.path.join(dirname, category)#用于路径拼接，拼接文件路径
+        cat_dir = os.path.join(dirname, category)  # 用于路径拼接，拼接文件路径
         if not os.path.isdir(cat_dir):
             continue
         files = os.listdir(cat_dir)
@@ -40,11 +41,11 @@ def save_file(dirname):#data/thucnews
         for cur_file in files:
             filename = os.path.join(cat_dir, cur_file)
             content = _read_file(filename)
-            if count < 5000:#从每类中各写5k个到train set。一共有10类，所以train set含有5k*10条数据
+            if count < 5000:  # 从每类中各写5k个到train set。一共有10类，所以train set含有5k*10条数据
                 f_train.write(category + '\t' + content + '\n')
-            elif count < 6000:#1k个到test.set
+            elif count < 6000:  # 1k个到test.set
                 f_test.write(category + '\t' + content + '\n')
-            else:#rest（500）条写入validation test
+            else:  # rest（500）条写入validation test
                 f_val.write(category + '\t' + content + '\n')
             count += 1
 
@@ -54,15 +55,14 @@ def save_file(dirname):#data/thucnews
     f_test.close()
     f_val.close()
 
+
 if __name__ == '__main__':
     '''
     if _name_ == '_main_':的作用
     当.py文件被直接运行时，if __name__ == '__main__'之下的代码块将被运行；
     当.py文件以模块形式被导入时，if __name__ == '__main__'之下的代码块不被运行。
     '''
-    save_file('data/thucnews')#需要自行在https://github.com/gaussic/text-classification-cnn-rnn下载
+    save_file('data/thucnews')  # 需要自行在https://github.com/gaussic/text-classification-cnn-rnn下载
     print(len(open('data/cnews/cnews.train.txt', 'r', encoding='utf-8').readlines()))
     print(len(open('data/cnews/cnews.test.txt', 'r', encoding='utf-8').readlines()))
     print(len(open('data/cnews/cnews.val.txt', 'r', encoding='utf-8').readlines()))
-
-
